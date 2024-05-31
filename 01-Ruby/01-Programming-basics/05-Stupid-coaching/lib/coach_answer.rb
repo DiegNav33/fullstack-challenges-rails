4151074bfe57e417c64b837c00fedac2b9d04877
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-05-31 23:33:25 +0800

    Exercise 5 done. You can talk with your coach, try

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,33 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    return ""
+  elsif your_message.include?("?")
+    return "Silly question, get dressed and go to work!"
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  motivation_message = "I can feel your motivation!"
+
+  if your_message.upcase == your_message
+    if your_message == "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message.include?("?")
+      return "#{motivation_message} Silly question, get dressed and go to work!"
+    else
+      return "#{motivation_message} I don't care, get dressed and go to work!"
+    end
+  else
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/tempCodeRunnerFile.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/tempCodeRunnerFile.rb
@@ -4,16 +4,7 @@ puts "Ecris un message a ton coach"
 student_message = gets.chomp
 puts "#{coach_answer_enhanced(student_message)}"
 
-
-=begin
 while student_message != "I am going to work right now!"
-  puts "Continue de parler avec ton coach, ce n'est pas suffisant !"
-  student_message = gets.chomp
-  puts coach_answer_enhanced(student_message)
-end
-=end
-
-until student_message == "I am going to work right now!" || student_message == "I AM GOING TO WORK RIGHT NOW!"
   puts "Continue de parler avec ton coach, ce n'est pas suffisant !"
   student_message = gets.chomp
   puts coach_answer_enhanced(student_message)
