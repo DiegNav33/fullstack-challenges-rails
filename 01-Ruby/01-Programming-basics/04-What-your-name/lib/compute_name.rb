d6dc7e4cad8c73cc694fd24ef9c0bc99c28d1f8b
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-05-31 19:04:47 +0800

    Exercice 4 done

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,3 +1,4 @@
 def compute_name(first_name, middle_name, last_name)
   # TODO: return full name using string interpolation
+  return "#{first_name} #{middle_name} #{last_name}"
 end
--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,7 +1,14 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
+puts "What's your fist name ?"
+first_name = gets.chomp
 # TODO: ask for the middle name
+puts "What's your middle name ?"
+middle_name = gets.chomp
 # TODO: ask for the last name
+puts "What's your last name ?"
+last_name = gets.chomp
 # TODO: Print a greeting to the user with the full name concatenated,
+print "Hello, #{first_name} #{middle_name} #{last_name} ! "
 #       something like "Hello, Boris Paillard"
