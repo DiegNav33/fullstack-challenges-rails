a9014f9432f03e9b120c1843b272b3891ba37db2
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-01 23:14:52 +0800

    Exercise 3 in 2nd part done. You can add students at le wagon session

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,33 @@ require_relative "wagon_sort"
 #       - `gets`:  https://ruby-doc.org/core-3.1.2/Kernel.html#method-i-gets
 #       - `chomp`: https://ruby-doc.org/core-3.1.2/String.html#method-i-chomp
 
+students = []
+puts "Ecris un prénom d'étudiant à ajouter au Wagon !"
+student_name = gets.chomp
+students.push(student_name) unless student_name.empty?
 
+while student_name != ""
+  puts "Ecris un prénom d'étudiant à ajouter au Wagon, si tu as finis appui entrer sans rien mettre !"
+  student_name = gets.chomp
+  students.push(student_name) unless student_name.empty?
+end
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+
+if student_name == ""
+  if students.length == 1
+    puts "Congratulations! Your Wagon has #{students.length} student :"
+  elsif students.empty?
+    puts "Sorry your Wagon still doesnt have any students !"
+    exit
+  else
+    puts "Congratulations! Your Wagon has #{students.length} students :"
+  end
+  formated_array = wagon_sort(students)
+  if formated_array.length <= 2
+    puts formated_array.join(" and ")
+  else
+    puts formated_array[0..-2].join(", ") + " and " + formated_array[-1]
+  end
+end
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,4 @@
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  students = students.sort_by{|element| element.downcase}
 end
