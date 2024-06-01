d4d7a95bcf2add19f9fb97a657782abc98f7ad27
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-01 17:18:47 +0800

    Exercise 2 done in 2nd part. loop for..end and while..end

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,32 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  if min > max
+    return -1
+  end
+
+  sum = 0
+
+  for n in min..max
+    sum = sum + n
+  end
+
+  return sum
+
 end
+
 
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  if min > max
+    return -1
+  end
+
+  sum = 0
+
+  while min <= max
+    sum = sum + min
+    min += 1
+  end
+
+  return sum
 end
