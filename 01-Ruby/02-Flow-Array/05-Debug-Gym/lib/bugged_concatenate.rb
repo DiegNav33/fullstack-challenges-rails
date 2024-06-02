f2fb6e8f92ed9c2a6f272dfe9237e9d0799230e9
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-02 14:57:51 +0800

    Exercise 5 in 2nd part done. All clear in the code

--- 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,11 +1,12 @@
 def bugged_concatenate(array)
   concatenated_string = ""
   array.each do |element|
-    concatenated_string = concatenated_string + element
+    concatenated_string = concatenated_string + element.to_s
   end
   return concatenated_string.upcase
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  my_array = [1, "9", 84, " ", "George Orwell"]
+  bugged_concatenate(my_array)
 end
