58488b304f5e276dc996a12c806691dcfde10ba8
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-03 16:37:53 +0800

    Exercise 5 in 3rd part done. Create splitters -> array

--- 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
@@ -2,6 +2,22 @@ def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+
+  array_size = []
+  array_other = []
+
+  array.each do |element|
+    if element.length == size
+      array_size << element
+    else
+      array_other << element
+    end
+  end
+
+  array_size.sort!
+  array_other.sort!
+
+  [array_size, array_other]
 end
 
 def block_splitter(array)
@@ -9,4 +25,20 @@ def block_splitter(array)
   #       elements for which the given block yields true,
   #       the second containing all the other elements.
   #       No sort needed this time.
+
+  array_if_yield_true = []
+  array_if_no_block = []
+
+  array.each do |element|
+
+    if yield(element)
+      array_if_yield_true << element
+    else
+      array_if_no_block << element
+
+    end
+  end
+
+  [array_if_yield_true, array_if_no_block]
+
 end
