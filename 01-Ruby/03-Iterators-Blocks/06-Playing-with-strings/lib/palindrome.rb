e185e789627cc7affe4ff0d1f265b5e46713576b
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-03 17:11:35 +0800

    Exercise 6 in 3rd part done. Check if the string is a palindrome

--- 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,7 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
+  return false if a_string.nil? || a_string.empty?
+  new_string = a_string.downcase.gsub(/[^a-z0-9]/, "")
+  new_string == new_string.reverse
+
 end
