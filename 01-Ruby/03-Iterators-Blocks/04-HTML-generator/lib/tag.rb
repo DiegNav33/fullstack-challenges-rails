4533167adbeee1f0f7bd75aa103371f9737746eb
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-03 15:49:32 +0800

    Exercise 4 in 3rd part done. tag method created -> can create html tags

--- 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
+++ 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
@@ -1,4 +1,13 @@
 def tag(tag_name, attributes = nil)
   # TODO: Build HTML tags around content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  # attribute_html = ""
+  if attributes
+    attributes = " #{attributes[0]}=\"#{attributes[1]}\""
+  end
+  content = yield if block_given?
+  "<#{tag_name}#{attributes}>#{content}</#{tag_name}>"
 end
+
+puts tag("script", ["src", "https://kit.fontawesome.com/b5f95cb708.js"])
+puts tag("h1") {|content| "Titre principal"}
