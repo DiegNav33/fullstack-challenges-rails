9ead8b28b052d87df434874b2da7feef8ce03a53
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-04 15:55:27 +0800

    Exercise 1 in 4th part done. Calculate the total calories for your order regarding a hash

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
@@ -1,3 +1,17 @@
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this restaurant order
+
+  items = {
+  "Hamburger" => 250,
+  "Cheese Burger" => 300,
+  "Veggie Burger" => 540,
+  "Vegan Burger " => 350,
+  "Sweet Potatoes" => 230,
+  "Salad" => 15,
+  "Iced Tea" => 70,
+  "Lemonade" => 90
+  }
+
+
+  items[burger] + items[side] + items[beverage]
 end
