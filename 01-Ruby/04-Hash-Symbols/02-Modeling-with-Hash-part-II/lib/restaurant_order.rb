8bce6d022b03043c798bebbd33ea85205f1f6550
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-04 17:48:10 +0800

    Exercise 2 in 4th part done. Calculate the total calories of the orders

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
@@ -9,10 +9,27 @@ DISHES_CALORIES = {
   "Lemonade" => 90
 }
 
+MEALS = {
+  "Cheesy Combo" => ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+  "Veggie Combo" => ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+  "Vegan Combo" => ["Vegan Burger", "Salad", "Lemonade"]
+}
+
+
 def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
 def calories_counter(orders)
   # TODO: return number of calories for a less constrained order
+  total_calories = 0
+
+  orders.each do |element|
+    if DISHES_CALORIES.key?(element)
+      total_calories += DISHES_CALORIES[element]
+    else
+      total_calories += poor_calories_counter(MEALS[element][0], MEALS[element][1], MEALS[element][2])
+    end
+  end
+  total_calories
 end
