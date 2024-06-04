f1adeba8a028daee5ba8ffeafe22769abb06269f
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-04 17:59:26 +0800

    Exercise 3 in 4th part done. Quiz

--- 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
@@ -9,10 +9,12 @@
 
 def are_identical_symbols_same_objects?
   # TODO: Answer the question by making this method to return true or false
+true
 end
 
 def are_identical_strings_same_objects?
   # TODO: Answer the question by making this method to return true or false
+  false
 end
 
 
@@ -22,16 +24,25 @@ end
 
 def convert_string_to_symbol(a_string)
   # TODO: return the symbol version of the parameter `a_string` passed to this method
+  a_string.to_sym
 end
 
 def convert_symbol_to_string(a_symbol)
   # TODO: return the string version of the parameter `a_symbol` passed to this method
+  a_symbol.to_s
 end
 
 def me
   # TODO: return a Hash representing yourself, with keys such as age and name
+
+  myself = {
+    name: "Diego",
+    age: 28
+  }
 end
 
 def fruits
   # TODO: return an array of fruits
+
+  fruits = %w[Apple Banana Orange]
 end
