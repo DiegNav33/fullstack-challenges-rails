c331c6010cf6c1f891fae83dd584a39ac4725c61
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-05 16:29:20 +0800

    Exercise 5 in 4th part done. transform an array to an hash

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,17 @@
 def array_to_hash(array)
   # TODO: implement the method :)
+  hash = {}
+  array.each_with_index do |element, index|
+    if block_given?
+      key = yield(index)
+      hash[key] = element
+    else
+      hash[index.to_s] = element
+    end
+  end
+  hash
 end
+
+
+# Avec Bloc : Si un bloc est donné, le bloc est appelé avec l'index et le résultat est utilisé comme clé.
+# Sans Bloc : Si aucun bloc n'est donné, l'index est converti en chaîne de caractères et utilisé comme clé.
