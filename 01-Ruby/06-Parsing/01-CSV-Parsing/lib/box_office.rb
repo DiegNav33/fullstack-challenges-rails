79acd6c2cc2505538553adf23d123b85180f0486
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-06 12:59:06 +0800

    Exercise 1 in 6th part done. Manipulating CSV files, to transform in array of hash

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -2,4 +2,20 @@ require 'csv'
 
 def most_successful(number, max_year, file_path)
   # TODO: return an array of most successful movies before `max_year`
+  movies_list = []
+
+  CSV.foreach(file_path, encoding: 'UTF-8') do |element|
+    name = element[0]
+    year = element[1].to_i
+    earnings = element[2].to_i
+
+    if year < max_year
+      movies_list << { name: name, year: year, earnings: earnings }
+    end
+  end
+
+  sorted_list = movies_list.sort_by { |movie| movie[:earnings] }.reverse
+  sorted_list.take(number)
 end
+
+puts most_successful(5, 1940, "lib/movies.csv")
