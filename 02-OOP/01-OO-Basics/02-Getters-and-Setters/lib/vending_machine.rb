d953aac681b40bee492ca93ad24367b749c8a154
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-10 16:01:36 +0800

    Exercise 2 in OOP done. Vending machine ready to be used

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -1,5 +1,7 @@
 class VendingMachine
   # TODO: add relevant getter/setter to this class to make the scenarios work properly.
+    attr_reader :snack_price_cents
+    attr_accessor :snack_count, :user_balance_cents
 
   def initialize(snack_price_cents, snack_count)
     @user_balance_cents = 0
@@ -10,10 +12,24 @@ class VendingMachine
   def insert_coin(input_cents)
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user inserts a coin?
+    @user_balance_cents += input_cents
   end
 
   def buy_snack
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user pushes a button to buy a Snack?
+    if @snack_count < 1
+      puts "Sorry out of stock !"
+      return
+    end
+
+    if @user_balance_cents >= @snack_price_cents
+      puts "Thanks for your purchase !"
+      @snack_count -= 1
+      @user_balance_cents -= @snack_price_cents
+    else
+      puts "Sorry your balance is too low, insert more coins to purchase.\n"
+      return
+    end
   end
 end
