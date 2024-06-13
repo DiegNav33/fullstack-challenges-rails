187e35b156ad2f737887687d8d75e5a9b783e1a6
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-13 13:47:09 +0800

    Optional exercice 2 in OOP Done. Method missing implement

--- 02-OOP/01-OO-Basics/Optional-02-Method-Missing/lib/uber_hash.rb
+++ 02-OOP/01-OO-Basics/Optional-02-Method-Missing/lib/uber_hash.rb
@@ -1,2 +1,18 @@
 class UberHash
+
+  def initialize
+    @properties = {}
+  end
+
+  def method_missing(method_name, *args)
+    property_name = method_name.to_s
+    if property_name.end_with?("=")
+      # Si le nom de la méthode se termine par "=", c'est une définition de propriété
+      property_name_without_equal = property_name.chomp("=")
+      @properties[property_name_without_equal] = args.first
+    else
+      # Sinon, c'est un accès à une propriété
+      @properties[method_name.to_s]
+    end
+  end
 end
