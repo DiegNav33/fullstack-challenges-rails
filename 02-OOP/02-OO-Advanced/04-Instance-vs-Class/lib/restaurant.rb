6a086aed42c263318b3e672637cbc0ec4397168e
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-13 21:32:48 +0800

    Exercise 4 in 2nd part of OOP done. Instance vs Class

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,25 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
 
+  attr_accessor :city, :name, :average_rating
+
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = 0.0  # Initialisation de la moyenne des notes à 0.0
+    @total_ratings = 0     # Compteur pour le nombre total de notes
   end
 
   # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @average_rating = ((@average_rating * @total_ratings) + new_rate) / (@total_ratings + 1.0)
+    @total_ratings += 1
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select do |restaurant|
+      restaurant.city == city
+    end
+  end
 end
