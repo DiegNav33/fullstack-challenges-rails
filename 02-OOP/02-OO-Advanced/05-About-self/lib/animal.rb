62f721f38255d09591e80abe3801407795f42ea5
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-14 09:51:37 +0800

    Exercise 5 in 2nd part of OOP done. Self

--- 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
@@ -2,21 +2,25 @@ class Animal
   def name(name)
     @name = name
     # TODO: what should this method return?
+    self
   end
 
   def species(species)
     @species = species
     # TODO: what should this method return?
+    self
   end
 
   def color(color)
     @color = color
     # TODO: what should this method return?
+    self
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     # TODO: what should this method return?
+    self
   end
 
   def to_s
--- 02-OOP/02-OO-Advanced/05-About-self/lib/winning_combination.rb
+++ 02-OOP/02-OO-Advanced/05-About-self/lib/winning_combination.rb
@@ -2,15 +2,18 @@ module DemoModule
   class DemoClass
     def self.introduce
       # TODO: choose return between 1, 2, 3
+      2
     end
 
     def introduce
       # TODO: choose return between 1, 2, 3
+      3
     end
   end
 
   def self.introduce
     # TODO: choose return between 1, 2, 3
+    1
   end
 end
 
@@ -21,3 +24,30 @@ def winning_combination?
   # Next line should return true!
   [first, second, third] == [1, 2, 3]
 end
+
+
+module MyModule
+  class MyClass
+    def self.class_method
+      "Class method"
+    end
+
+    def instance_method
+      "Instance method"
+    end
+  end
+
+  def self.module_method
+    "Module method"
+  end
+end
+
+# Appel de la méthode de module
+puts MyModule.module_method        # Affiche "Module method"
+
+# Appel de la méthode de classe
+puts MyModule::MyClass.class_method  # Affiche "Class method"
+
+# Appel de la méthode d'instance
+instance = MyModule::MyClass.new
+puts instance.instance_method       # Affiche "Instance method"
