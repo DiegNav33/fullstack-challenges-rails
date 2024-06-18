ff08204eccbb4a57cd717fc20dcb08a656e72a42
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-18 16:09:04 +0800

    Exercise 3 in 3rd part in OOP done. Cookbook with csv

--- 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/controller.rb
+++ 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/controller.rb
@@ -1,1 +1,32 @@
 #  TODO: Define your Controller Class here, to orchestrate the other classes
+require_relative "cookbook"
+require_relative "recipe"
+require_relative "view"
+
+class Controller
+
+  def initialize(cookbook)
+    @cookbook = cookbook
+    @view = View.new
+  end
+
+  def list
+    recipes = @cookbook.all
+    @view.display_list(recipes)
+  end
+
+  def add
+    name = @view.ask_user_for_name
+    description = @view.ask_user_for_description
+    recipe = Recipe.new(name, description)
+    @cookbook.create(recipe)
+  end
+
+  def remove
+    recipes = @cookbook.all
+    @view.display_list(recipes)
+    recipe_index = @view.ask_user_for_index
+    @cookbook.destroy(recipe_index)
+  end
+
+end
--- 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/cookbook.rb
@@ -1,1 +1,45 @@
 # TODO: Implement the Cookbook class that will be our repository
+
+require "csv"
+
+class Cookbook
+
+  def initialize(csv_file_path)
+    @recipes = []
+    @csv_file_path = csv_file_path
+    load_csv
+  end
+
+  def all
+    @recipes
+  end
+
+  def create(recipe)
+    @recipes << recipe
+    save_csv
+  end
+
+  def destroy(recipe_index)
+    @recipes.delete_at(recipe_index)
+    save_csv
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file_path) do |row|
+      name = row[0]
+      description = row[1]
+      recipe = Recipe.new(name, description)
+      create(recipe)
+    end
+  end
+
+  def save_csv
+    CSV.open(@csv_file_path, "wb") do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe.name, recipe.description]
+      end
+    end
+  end
+end
