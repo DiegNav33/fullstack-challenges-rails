986bac76ef80058c6a3cd18af740c4b336d5ebaa
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-25 11:15:04 +0800

    3rd commit of food delivery. new model added (customer)

--- 02-OOP/04-Food-Delivery-Day-One/01-Food-Delivery/app.rb
+++ 02-OOP/04-Food-Delivery-Day-One/01-Food-Delivery/app.rb
@@ -6,7 +6,7 @@ require_relative "app/repositories/meal_repository"
 require_relative "app/controllers/meals_controller"
 require_relative "router"
 
-MEAL_CSV_FILE = File.join(File.dirname(__dir__), "meals.csv")
+MEAL_CSV_FILE = File.join(__dir__, "data/meals.csv")
 meals_repo = MealRepository.new(MEAL_CSV_FILE)
 meals_controller = MealsController.new(meals_repo)
 router = Router.new(meals_controller)
