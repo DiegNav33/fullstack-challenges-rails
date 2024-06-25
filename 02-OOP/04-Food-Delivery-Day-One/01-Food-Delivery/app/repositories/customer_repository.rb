baf8c1afa9774793a41ce3912e332a1cf1ad2515
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-25 11:44:43 +0800

    4th commit of food delivery. Customer repo added

---
+++ 02-OOP/04-Food-Delivery-Day-One/01-Food-Delivery/app/repositories/customer_repository.rb
@@ -0,0 +1,46 @@
+require "csv"
+require_relative "../models/customer"
+
+class CustomerRepository
+
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @customers = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def create(customer)
+    customer.id = @next_id
+    @next_id += 1
+    @customers << customer
+    save_csv
+  end
+
+  def all
+    @customers
+  end
+
+  def find(id)
+    @customers.find {|customer| customer.id == id}
+  end
+
+  private
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << %w[id name address]
+      @customers.each do |customer|
+        csv << [customer.id, customer.name, customer.address]
+      end
+    end
+  end
+
+  def load_csv
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |row|
+      row[:id] = row[:id].to_i
+      @customers << Customer.new(row)
+    end
+    @next_id = @customers.empty? ? 1 : @customers.last.id + 1
+  end
+end
