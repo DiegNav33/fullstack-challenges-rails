b09c0421de9bde0f3a1cb632205710e382b57004
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-25 22:29:32 +0800

    Order model added

---
+++ 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/app/models/order.rb
@@ -0,0 +1,21 @@
+class Order
+
+  attr_accessor :id
+  attr_reader :meal, :customer, :employee, :delivered
+
+  def initialize(attributes = {})
+  @id = attributes[:id]
+  @meal = attributes[:meal]
+  @customer = attributes[:customer]
+  @employee = attributes[:employee]
+  @delivered = attributes[:delivered]
+  end
+
+  def delivered?
+    @delivered == true
+  end
+
+  def deliver!
+    @delivered = true
+  end
+end
