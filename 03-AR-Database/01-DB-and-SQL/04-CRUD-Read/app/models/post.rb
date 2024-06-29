12936c69c00ffb9817fd58b85a3c0e433acc78d9
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-06-29 22:31:55 +0800

    R in CRUD done. few methods added in the Post model

--- 03-AR-Database/01-DB-and-SQL/04-CRUD-Read/app/models/post.rb
+++ 03-AR-Database/01-DB-and-SQL/04-CRUD-Read/app/models/post.rb
@@ -2,17 +2,42 @@
 # No need to create it, you can just use it!
 
 class Post
+
+  attr_reader :id, :votes
+  attr_accessor :title, :url
+
   def initialize(attributes = {})
-    # TODO
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @url = attributes[:url]
+    @votes = attributes[:votes]
   end
 
   def self.find(id)
     DB.results_as_hash = true
-    # TODO
+    query = "SELECT * FROM posts WHERE id = ? LIMIT 1"
+    result = DB.execute(query, id)[0]
+    return nil if result.nil?
+    build_record(result)
   end
 
   def self.all
     DB.results_as_hash = true
-    # TODO
+    result = DB.execute("SELECT * FROM posts")
+    result.map do |row|
+      build_record(row)
+    end
+  end
+
+  private
+
+  def self.build_record(row)
+    attributes = {
+      id: row["id"],
+      title: row["title"],
+      url: row["url"],
+      votes: row["votes"]
+    }
+    Post.new(attributes)
   end
 end
