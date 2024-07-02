09c6cd3aeef380c2b97710583c6026cc09571383
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-02 22:24:15 +0800

    seed added with 100 fake datas generated with faker gem

--- 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,13 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
+
+puts "Creating 100 fake posts..."
+
+100.times do
+  post = Post.new(
+    title: Faker::Commerce.product_name,
+    url: Faker::Internet.url,
+    votes: (0..1000).to_a.sample
+  )
+  post.save!
+end
