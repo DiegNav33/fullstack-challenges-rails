f638e63d976093abdba272794d40d7010670497f
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-02 15:15:12 +0800

    Schema created, 2 migrations : create_table and add_column

--- 03-AR-Database/02-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string :title
+      t.string :url
+      t.timestamps
+    end
   end
 end
