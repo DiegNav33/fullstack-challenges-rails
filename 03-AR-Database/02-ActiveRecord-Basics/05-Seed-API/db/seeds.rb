e54c5d81f0522aeeed8c4f1b228027082d361f62
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-02 23:05:20 +0800

    Seed API used. Get infos from the API of hacker news.

--- 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -1,4 +1,21 @@
 require "json"
 require "rest-client"
 
-# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+def story_url(id)
+  "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+end
+#JSON.parse va transformer la chaine JSON en objet ruby comme un array ou hash et Resclient.get retourne une chaine JSON representant un tableau de posts dans ce cas la
+post_ids = JSON.parse(RestClient.get(TOP_STORIES_URL))
+
+post_ids.take(10).each do |id|
+  hacker_news_post = JSON.parse(RestClient.get(story_url(id)))
+  post = Post.new(
+    title: hacker_news_post["title"],
+    url: hacker_news_post["url"],
+    votes: hacker_news_post["score"]
+  )
+  post.save
+  puts "[#{post.votes}] #{post.title} - #{post.url}"
+end
