09380d8a75a7717cef2f1477e3ab01c6c4ae5563
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-02 17:54:26 +0800

    Active recod methods on controller added

--- 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -9,25 +9,45 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    posts = Post.all
+    @view.display(posts)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    title = @view.ask_user_title
+    url = @view.ask_user_url
+    Post.create(title: title, url: url)
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    index
+    id_selected = @view.ask_user_id
+    new_title = @view.ask_user_title
+    new_url = @view.ask_user_url
+    post = Post.find(id_selected)
+    post.update(title: new_title.empty? ? post.title : new_title, url: new_url.empty? ? post.url : new_url)
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    index
+    id_selected = @view.ask_user_id
+    post = Post.find(id_selected)
+    post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    index
+    id_selected = @view.ask_user_id
+    post = Post.find(id_selected)
+    new_rate = post.votes + 1
+    post.update(votes: new_rate)
   end
 end
--- 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,24 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id}. #{post.title}: #{post.url} => #{post.votes} vote(s)"
+    end
+  end
+
+  def ask_user_title
+    puts "Enter a title: "
+    return gets.chomp
+  end
+
+  def ask_user_url
+    puts "Enter an URL: "
+    return gets.chomp
+  end
+
+  def ask_user_id
+    puts "Pick with the id!"
+    return gets.chomp.to_i
+  end
 end
