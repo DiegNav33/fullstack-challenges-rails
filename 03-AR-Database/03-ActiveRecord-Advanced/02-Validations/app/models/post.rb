4bd60f443bbb261ea16d793ce906e1874257f3b6
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-03 20:38:23 +0800

    Some validations added to the models, and also callback methods for user

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :title, presence: true, length: {minimum: 5}, uniqueness: {case_sensitive: false}
+  validates :user, presence: true
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is invalid" }
 end
--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -2,5 +2,23 @@ class User < ActiveRecord::Base
   has_many :posts
 
   # TODO: Add some validation
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/ }
   # TODO: Add some callbacks
+
+  # Callback pour enlever les espaces blancs au début et à la fin de l'email
+  before_validation :strip_whitespace_from_email
+
+  # Callback après la création pour envoyer un email de bienvenue
+  after_create :send_welcome_email
+
+  private
+
+  def strip_whitespace_from_email
+    self.email = email.strip if email.present?
+  end
+
+  def send_welcome_email
+    FakeMailer.instance.mail(email, "Welcome to Hacker News!")
+  end
 end
