e7fe84d496d67eaac84173a3cc183be285065605
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-03 18:38:25 +0800

    Associations done between Posts and Users (belongs_to and has_many). Seed created to add users and posts

--- 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,20 @@
 # TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.new(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email
+  )
+  user.save
+
+  (5..10).to_a.sample.times do
+    post = Post.new(
+      title: Faker::Commerce.product_name,
+      url: Faker::Internet.url,
+      votes: (0..1000).to_a.sample,
+      user: user
+    )
+    post.save
+  end
+end
