00b1de403b6bfd09deeae68f954d8120bb33f2f0
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-04 18:28:14 +0800

    First Sinatra app created, Hacker News Like

--- 03-AR-Database/03-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/03-Sinatra/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  # TODO: Copy-paste your code from previous exercise
+  # TODO: Add some validation
+  validates :title, presence: true, length: {minimum: 5}, uniqueness: {case_sensitive: false}
+  validates :user, presence: true
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is invalid" }
 end
--- 03-AR-Database/03-ActiveRecord-Advanced/03-Sinatra/lib/app.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/03-Sinatra/lib/app.rb
@@ -12,13 +12,39 @@ after do
   ActiveRecord::Base.connection.close
 end
 
+# Route pour la page d'accueil affichant tous les posts
 get "/" do
-  # TODO
-  # 1. fetch posts from database.
-  # 2. Store these posts in an instance variable
-  # 3. Use it in the `app/views/posts.erb` view
-
+  @posts_desc = Post.all.order(votes: :desc)
+  @post = Post.new
   erb :posts # Do not remove this line
 end
+
+post "/posts" do
+  title = params[:title]
+  url = params[:url]
+  user = User.first
+  @post= Post.create(title: title, url: url, user: user)
+
+  if @post.persisted?
+    redirect to('/')
+  else
+    erb :new_post
+  end
+end
+
+put "/posts/:id/upvote" do
+  post = Post.find(params[:id])
+  post.votes += 1
+  post.save
+  redirect '/'
+end
+
+delete "/posts/:id/delete" do
+  post = Post.find(params[:id])
+  post.destroy
+  post.save
+  redirect "/"
+end
+
 
 # TODO: add more routes to your app!
