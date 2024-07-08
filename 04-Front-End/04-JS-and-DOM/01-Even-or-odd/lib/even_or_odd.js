5e26aa2e7d2458a5dca353f8af51fde0cc96dfab
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-08 21:20:02 +0800

    Even or odd methods to start JS

--- 04-Front-End/04-JS-and-DOM/01-Even-or-odd/lib/even_or_odd.js
+++ 04-Front-End/04-JS-and-DOM/01-Even-or-odd/lib/even_or_odd.js
@@ -5,7 +5,13 @@
 import runChallenges from "../spec/even_or_odd_examiner.js";
 
 export const evenOrOdd = (number) => {
-  // TODO: this should return "even" if the number is even, "odd" otherwise
+  // TODO: this should return "even" if the number is even, "odd" otherwise``
+  if (number % 2 === 0) {
+    console.log("even");
+    return "even";
+  }
+  console.log("odd");
+  return "odd";
 };
 
 // Checking exercise answers. DO NOT MODIFY THIS LINE.
