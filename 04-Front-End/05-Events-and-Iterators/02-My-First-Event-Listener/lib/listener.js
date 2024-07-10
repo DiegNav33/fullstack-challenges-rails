dccd4272b898be0a6521ec484138f57b82df0a36
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-10 13:05:47 +0800

    First event listener with a button

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,16 @@
 // TODO: React to a click on the button!
+
+const button = document.getElementById("clickme");
+
+button.addEventListener("click", (event) => {
+  const audio = new Audio("../sound.mp3");
+  button.classList.add("disabled");
+  button.innerText = "Bingo!";
+  audio.play();
+
+  // Exemple de rétablissement après 3 secondes
+  setTimeout(() => {
+    button.classList.remove("disabled");
+    button.innerText = "Click me!";
+  }, 3000); // 3000 millisecondes (3 secondes)
+});
