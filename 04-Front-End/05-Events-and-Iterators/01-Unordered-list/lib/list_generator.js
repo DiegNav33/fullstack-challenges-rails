9a1a0a93aef65c12e82b7b2573a2118a7b3ee823
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-10 12:37:50 +0800

    List generated with js and the use of .map on array

--- 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
+++ 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
@@ -6,10 +6,13 @@ import runChallenges from "../spec/list_generator_examiner.js";
 
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  const list = items.map(item => listItem(item)).join("");
+  return `<ul class="list-group">${list}</ul>`;
 };
 
 // Do not remove these lines:
@@ -17,6 +20,14 @@ if (typeof window === "object") {
   document.addEventListener("DOMContentLoaded", () => {
     // TODO: call unorderedList() passing the right element
     // TODO: display the list in the browser
+    const groceries = [
+      'apple', 'banana', 'orange', 'grape', 'strawberry', 'watermelon',
+      'kiwi', 'pineapple', 'blueberry', 'peach', 'pear', 'raspberry'
+    ];
+    const list = document.getElementById("list");
+    list.innerHTML = unorderedList(groceries);
+    const test = groceries.map((item, index) => `${index + 1}. ${item}\n`).join("");
+    console.log(test);
   });
 }
 
