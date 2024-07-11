d87372182bd17c36421fac3a1bcd6c5089f8987f
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-11 23:07:33 +0800

    Flex panels created

--- 04-Front-End/05-Events-and-Iterators/Optional-02-Flex-Panels/lib/panels.js
+++ 04-Front-End/05-Events-and-Iterators/Optional-02-Flex-Panels/lib/panels.js
@@ -1,1 +1,13 @@
-console.log('panels.js loaded');
+document.addEventListener("DOMContentLoaded", () => {
+  const allPanels = document.querySelectorAll(".panel");
+  allPanels.forEach(panel => {
+    panel.addEventListener("click", (event) => {
+      allPanels.forEach(otherPanel => {
+        if (otherPanel !== panel) {
+          otherPanel.classList.remove("panelOpened");
+        }
+      });
+      panel.classList.toggle("panelOpened");
+    });
+  });
+});
