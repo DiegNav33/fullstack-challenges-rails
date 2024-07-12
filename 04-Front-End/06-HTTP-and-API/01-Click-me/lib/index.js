f2518904fe8e07cd120a58644e25ffb4346a985d
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-12 11:43:33 +0800

    Click me button added

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -2,6 +2,11 @@ const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+
+  const button = document.querySelector(".btn");
+  button.addEventListener("click", (event) => {
+    alert("Thank you!");
+  });
 };
 
 displayAlertOnButtonClick(); // Do not remove!
