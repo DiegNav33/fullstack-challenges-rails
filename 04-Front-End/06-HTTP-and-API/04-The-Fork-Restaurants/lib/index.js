a5c6948e1d9ad5c31ddf1a44f9ac3a8781d1fa24
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-12 19:55:48 +0800

    Fork API used

--- 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
@@ -1,20 +1,46 @@
-// Todo: Use the Fork Api to get a
-// filtered list of restaurants, depending on
-// the category selected (and as a bonus, the location)
-
+const restaurantList = document.getElementById("restaurant-list");
+const locationInput = document.getElementById("location");
 const searchForm = document.getElementById("searchForm");
 
-// Todo: select the HTML elements you need
+const buildSearchUrl = (category, location) => {
+  let url = "https://the-fork.api.lewagon.com/api/v1/";
+  url += `restaurants?category=${category}`;
+  if (location) {
+    url += `&location=${location}`;
+  }
+  return url;
+};
+
+const insertResults = (restaurants) => {
+  restaurantList.innerHTML = ""; // Réinitialiser la liste
+
+  if (restaurants.length === 0) {
+    restaurantList.innerHTML = '<li class="list-group-item">Aucun restaurant trouvé pour cette catégorie et cet emplacement.</li>';
+  } else {
+    restaurants.forEach((restaurant) => {
+      // Pour chaque restaurant dans le tableau `restaurants`, exécute le bloc de code suivant :
+      // Créer un nouvel élément <li> pour chaque restaurant
+      const listItem = document.createElement('li');
+      // Ajouter la classe CSS 'list-group-item' à l'élément <li>
+      listItem.classList.add('list-group-item');
+      // Définir le texte contenu de l'élément <li> sur le nom du restaurant
+      listItem.textContent = `${restaurant.name} at ${restaurant.address}`;
+      // Ajouter l'élément <li> créé à la fin de la liste <ul> avec l'id 'restaurant-list'
+      restaurantList.appendChild(listItem);
+    });
+  }
+};
 
 
 searchForm.addEventListener("submit", (event) => {
-  // Todo: Find the category selected and build the URL you will send your request to
-
-  // Todo: Replace "the-endpoint-url" with the URL you built
-  fetch("the-endpoint-url")
+  event.preventDefault();
+  const category = document.querySelector("input[name='category']:checked").value;
+  const location = locationInput.value;
+  const endpointUrl = buildSearchUrl(category, location);
+  fetch(endpointUrl)
     .then(response => response.json())
     .then((data) => {
-      // Todo: Insert results into the list
-
+      // console.log(data);
+      insertResults(data);
     });
 });
