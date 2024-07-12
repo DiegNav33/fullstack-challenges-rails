546719cf9ae5e1c165ec2b5c0201a253673a0229
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-12 11:59:22 +0800

    Favourite sports created

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,10 @@
 // TODO: write your code here!
+document.addEventListener("DOMContentLoaded", () => {
+  const allElt = document.querySelectorAll(".clickable");
+  allElt.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      element.classList.toggle("active");
+      console.log("Activated!");
+    });
+  });
+});
