4bf7f37769d919c8830f6c2e60fcbbac90712305
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-13 17:57:38 +0800

    autocomplete on an input done

--- 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,34 @@
 // TODO: Autocomplete the input with AJAX calls.
+const url = "https://dictionary.lewagon.com/autocomplete/";
+const ul = document.getElementById("results");
+const input = document.getElementById("search");
+
+const fetchDatas = (event) => {
+  const inputValue = input.value.trim();
+
+  if (!inputValue) {
+    // Si l'input est vide, vider la liste et sortir de la fonction
+    ul.innerHTML = "";
+    return;
+  }
+
+  const urlBuilt = `${url}${inputValue}`;
+  fetch(urlBuilt)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (data.words.length <= 0) {
+      ul.innerText = "Sorry no words found!";
+    } else {
+      const words = data.words.slice(0, 5); // Prend les 5 premiers éléments du tableau words
+      ul.innerHTML = "";
+      words.forEach((word) => {
+        const li = document.createElement("li");
+        li.innerText = word;
+        ul.appendChild(li);
+      });
+    }
+  });
+};
+
+input.addEventListener("keyup", fetchDatas);
