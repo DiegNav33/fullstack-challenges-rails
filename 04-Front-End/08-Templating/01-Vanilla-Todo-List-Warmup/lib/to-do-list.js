7bd4b9aa61962777ba35b07f432b0f53a29487cc
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-14 14:12:20 +0800

    To do list warmup done

--- 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
+++ 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
@@ -2,13 +2,17 @@ const todos = [
   { title: "Code a to-do list", done: false },
   { title: "Eat breakfast", done: true },
   { title: "Do some exercise", done: false },
-  { title: "Water the plants", done: true }
+  { title: "Water the plants", done: true },
+  { title: "Groceries", done: false }
 ];
 
-// To-do item HTML 👇
-// <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
-//   <input class="d-flex form-check-input me-1" type="checkbox">
-//   <div>Replace with to-do title</div>
-// </div>
+const containerToDos = document.getElementById("todosContainer");
 
-// TODO: Dynamically generate a list of to-dos based on `todos` array, and display them
+todos.forEach((element) => {
+  const toDo = `
+  <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+    <input class="d-flex form-check-input me-1" type="checkbox" ${element.done ? "checked" : ""}>
+    <div>${element.title}</div>
+  </div>`;
+  containerToDos.insertAdjacentHTML("beforeend", toDo);
+});
