ae19b4976e58050181b4fd880a181462fe117127
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-14 14:50:18 +0800

    To do list with a template created

--- 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
+++ 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
@@ -5,4 +5,14 @@ const todos = [
   { title: "Water the plants", done: true }
 ];
 
+const template = document.getElementById("todoItemTemplate");
+const containerToDo = document.getElementById("todosContainer");
+
+todos.forEach((element) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".title").innerText = element.title;
+  clone.querySelector("input").checked = element.done;
+  containerToDo.appendChild(clone);
+});
+
 // TODO: Use the template in the `index.html` to dynamically generate a list based on `todos` array
