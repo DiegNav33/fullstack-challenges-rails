b5542f921fd132b8b1d577ee8cad41f441d25b3e
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-14 16:36:13 +0800

    To do list done with Vue.js

--- 04-Front-End/08-Templating/04-Todo-List-Vue/lib/to-do-list.js
+++ 04-Front-End/08-Templating/04-Todo-List-Vue/lib/to-do-list.js
@@ -3,7 +3,25 @@ import { createApp } from "vue";
 createApp({
   data() {
     return {
-      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌"
+      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌",
+      todos: [
+        { title: "Code a to-do list", done: false },
+        { title: "Eat breakfast", done: true },
+        { title: "Do some exercise", done: false },
+        { title: "Water the plants", done: true }
+      ],
+      newTodo: null
     };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo) {
+        this.todos.push({ title: this.newTodo.trim(), done: false });
+        this.newTodo = null;
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 }).mount('#todosContainer');
